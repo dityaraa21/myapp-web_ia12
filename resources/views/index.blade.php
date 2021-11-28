e9da25f860521e4e847fdba840cf1d38ee241ae9
dityaraa21 <adityarahman453@gmail.com>
2021-11-28 19:17:23 +0700

    Menambahkan folder dan file pada includes dan pages

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<!-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+    <h1>Setup Laravel Bootstrap Successfully <i class="fas fa-ad"></i></h1>
+       
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div> 
+    </div>
+        <script src="{{ Mix('js/app.js') }}"></script>
+    </body>
+</html> -->
+<!doctype html>
+<html>
+  <head>
+    @include('includes.head')
+  </head>
+  <body>
+      @include('includes.navbar')
+      <div class="wrap container">
+        <div id="main" class="row">
+          <div class="col-md-12">
+            @include('page.header')
+          </div>
+        </div>
+      </div>
+      @include('includes.footer')
+  </body>
+</html>
---
+++ resources/views/page/header.blade.php
@@ -0,0 +1,2 @@
+
+<h1 class="text-danger">Ini section header</h1>
